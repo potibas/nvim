acd8d139357d63f19f19ae870dc1266f9c29e89f
Potiguar Faga Catalan <me@potibas.dev>
2024-12-26 01:43:10 -0300

    Edit alternate file with ;a

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -8,3 +8,10 @@ features
   .expandCurrentDirectory('%%')
   .expandNeovimConfigDirectory('@@')
   .homeEndWithHL()
+
+local function map(modes, lhs, rhs, desc)
+  vim.keymap.set(modes, lhs, rhs, { desc = desc })
+end
+
+-- Common shortcuts
+map({ 'n', 'v' }, ';a', '<C-^>', 'Edit the alternate file')
